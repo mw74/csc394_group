a9cb5417cda88572fcdbb9ba21b7ba6cad21725d
Marc White <81710492+mw74@users.noreply.github.com>
2023-04-27 12:39:28 -0500

    Update settings.py

--- homework/settings.py
+++ homework/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i(jxg6g2m^2ci20cdpvrwsi-(lu6r1o)6lrmls^%svhooj1te0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
